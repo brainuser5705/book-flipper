f94d61de61d66ebc296ffd38745604ad2ff2bcb6
brainuser5705 <acl9213@rit.edu>
2022-03-27 11:27:17 -0400

    included page turner servo code

--- sketch_mar26c.ino
+++ sketch_mar26c.ino
@@ -1,27 +1,42 @@
 #include "Servo.h"
 
 #define BUTTON_PIN 2
-#define SERVO_PIN 12
+#define WHEEL_SERVO_PIN 12
+#define TURNER_SERVO_PIN 13
+
 
 int buttonState;
 Servo wheelServo;
+Servo turnerServo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
+
+  wheelServo.attach(WHEEL_SERVO_PIN);
+  turnerServo.attach(TURNER_SERVO_PIN);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   buttonState = digitalRead(BUTTON_PIN);
-  wheelServo.attach(SERVO_PIN);
   
   if (buttonState == HIGH){
+
+    wheelServo.attach(WHEEL_SERVO_PIN);
     wheelServo.write(70);
-    delay(100);
-  }else{
-    wheelServo.write(90);
+    delay(500);
+    wheelServo.write(90); // have to detach because it does not completely halt at 90
     wheelServo.detach();
+
+    delay(1000);
+
+    turnerServo.write(0);
+    delay(1000);
+    turnerServo.write(180);   
+    
+ 
   }
 }
