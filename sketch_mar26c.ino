5baf35c2f866939e22da94df265d8f93ef6df287
brainuser5705 <acl9213@rit.edu>
2022-03-30 21:05:57 -0400

    added potentiometer control code

--- sketch_mar26c.ino
+++ sketch_mar26c.ino
@@ -1,26 +1,52 @@
 #include "Servo.h"
 
-#define BUTTON_PIN 2
-#define WHEEL_SERVO_PIN 12
-#define TURNER_SERVO_PIN 13
+#define BUTTON_PIN                 5
+#define WHEEL_SERVO_PIN           13
+#define TURNER_SERVO_PIN          12
 
+#define LEFT_TILT_SERVO_PIN        2
+#define RIGHT_TILT_SERVO_PIN      11
+
+#define POTENT_PIN                A0
+#define MIN_POTENT                 0
+#define MAX_POTENT              1023
 
 int buttonState;
 Servo wheelServo;
 Servo turnerServo;
 
+Servo leftTiltServo;
+Servo rightTiltServo;
+
+int potentValue;
+int servoAngle;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
 
   wheelServo.attach(WHEEL_SERVO_PIN);
   turnerServo.attach(TURNER_SERVO_PIN);
   
+  leftTiltServo.attach(LEFT_TILT_SERVO_PIN);
+  rightTiltServo.attach(RIGHT_TILT_SERVO_PIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  flipPage();
+  tilt();
+}
+
+void tilt(){
+  potentValue = analogRead(A0);
+  servoAngle = map(potentValue, MIN_POTENT, MAX_POTENT, 0, 180);
+  rightTiltServo.write(servoAngle);
+  leftTiltServo.write(servoAngle);
+  delay(50);
+}
+
+void flipPage(){
   buttonState = digitalRead(BUTTON_PIN);
   
   if (buttonState == HIGH){
@@ -37,6 +63,5 @@ void loop() {
     delay(1000);
     turnerServo.write(180);   
     
- 
   }
 }
